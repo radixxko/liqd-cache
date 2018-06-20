71d5ad77df6a0c87d7b3c346f12bb0682eb144db
Tomas Korenko <radixxko@gmail.com>
2018-06-20 12:02:42 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# liqd-cache
+#Cache library
+
+[![Version npm](https://img.shields.io/npm/v/liqd-cache.svg)](https://www.npmjs.com/package/liqd-cache)
+[![Build Status](https://travis-ci.org/radixxko/liqd-cache.svg?branch=master)](https://travis-ci.org/radixxko/liqd-cache)
+[![Coverage Status](https://coveralls.io/repos/github/radixxko/liqd-cache/badge.svg?branch=master)](https://coveralls.io/github/radixxko/liqd-cache?branch=master)
---
+++ test/tests/cache.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const assert = require('assert' );
+
+const time_ms = () => (new Date()).getTime();
+
+const Cache = require('../../lib/cache');
+
+describe('- cache', function()
+{
+	it('should cache data', function( done )
+	{
+		let cache = new Cache();
+
+		assert.ok( cache.get( 'foo' ) === undefined );
+		assert.ok( cache.set( 'foo', 'bar' ) === cache );
+		assert.ok( cache.get( 'foo' ) === 'bar' );
+
+		assert.ok( cache.get( 'foo2' ) === undefined );
+		assert.ok( cache.set( 'foo2', 'bar2', Infinity, ( id, data ) =>
+		{
+			assert.ok( id === 'foo2' && data === 'bar2' );
+			assert.ok( cache.size === 0 );
+
+			setTimeout( done, 100 );
+		}) === cache );
+		assert.ok( cache.get( 'foo2', 100 ) === 'bar2' );
+
+		assert.ok( cache.get( 'foo' ) === 'bar' );
+
+		assert.deepStrictEqual( [ 'foo', 'foo2' ], Array.from(cache.keys()) );
+		assert.ok( cache.size === 2 );
+
+		assert.ok( cache.delete('bar') === undefined );
+		assert.ok( cache.size === 2 );
+
+		assert.ok( cache.delete('foo') === 'bar' );
+		assert.ok( cache.size === 1 );
+		assert.ok( cache.delete('foo') === undefined );
+
+		assert.deepStrictEqual( [ 'foo2' ], Array.from(cache.keys()) );
+		assert.ok( cache.delete('foo2') === 'bar2' );
+	});
+
+	it('should cache data with timeout', function( done )
+	{
+		let cache = new Cache();
+		let start = time_ms();
+
+		assert.ok( cache.get( 'foo' ) === undefined );
+		assert.ok( cache.set( 'foo', 'bar', 100 ) === cache );
+		assert.ok( cache.get( 'foo' ) === 'bar' );
+
+		assert.ok( cache.get( 'foo2' ) === undefined );
+		assert.ok( cache.set( 'foo2', 'bar2', 400, ( id, data ) =>
+		{
+			let now = time_ms();
+
+			assert.ok( id === 'foo2' && data === 'bar2' );
+			assert.ok( cache.size === 1 );
+
+			assert.ok( start + 500 < now + 50 && start + 500 > now - 50 );
+
+			setTimeout( done, 100 );
+		}) === cache );
+		assert.ok( cache.get( 'foo2' ) === 'bar2' );
+
+		assert.ok( cache.set( 'foo3', 'bar3', 300 ) === cache );
+		assert.ok( cache.set( 'foo4', 'bar4', 50 ) === cache );
+
+		assert.ok( cache.get( 'foo' ) === 'bar' );
+		assert.ok( cache.get( 'foo2' ) === 'bar2' );
+		assert.ok( cache.get( 'foo3' ) === 'bar3' );
+		assert.ok( cache.get( 'foo4' ) === 'bar4' );
+
+		assert.deepStrictEqual( [ 'foo', 'foo2', 'foo3', 'foo4' ], Array.from(cache.keys()) );
+		assert.ok( cache.size === 4 );
+
+		assert.ok( cache.set( 'foo4', 'bar4.4', 100 ) === cache );
+		assert.ok( cache.get( 'foo4' ) === 'bar4.4' );
+		assert.deepStrictEqual( [ 'foo', 'foo2', 'foo3', 'foo4' ], Array.from(cache.keys()) );
+		assert.ok( cache.size === 4 );
+
+		setTimeout( () =>
+		{
+			assert.ok( cache.size === 2 );
+			assert.ok( cache.delete('foo') === undefined );
+			assert.ok( cache.size === 2 );
+
+			assert.ok( cache.get('foo', 100) === undefined );
+			assert.ok( cache.get( 'foo2', 300 ) === 'bar2' );
+
+			assert.ok( cache.size === 2 );
+
+			assert.ok( cache.set( 'foo3', 'bar3' ) === cache );
+			assert.ok( cache.get( 'foo3' ) === 'bar3' );
+		},
+		200 );
+	});
+
+	it('should survive weird cases', function( done )
+	{
+		let cache = new Cache();
+
+		cache._expire('not_present');
+		cache.set('foo', 'bar');
+		cache._expire('foo');
+		cache.set('foo', 'bar', 100);
+		cache._expire('foo');
+
+		setTimeout( done, 250 );
+	});
+});
